98c477f0b9f618ed8b5df358da318196ae82efb6
rodfernando <hazardrodrigo@hotmail.com>
2021-12-21 21:22:29 -0300

    Atualização aula 09

--- pythonTeste/aula09a.py
+++ pythonTeste/aula09a.py
@@ -12,6 +12,13 @@ print(frase.upper().count('O'))
 print(frase.replace('Python', 'Android')) # se imprimir, estará com Python ainda. Nesse caso, se quiser alterar a frase colocar ela numa nova variável
 
 print('-' * 20)
+
 frase1 = "   Curso em Vídeo Python"
 print(len(frase1))
 print(len(frase1.strip()))
+print(frase1.find('Vídeo'))
+print(frase1.lower().find('vídeo'))
+print(frase1.split()) # cria uma lista separada por itens
+dividido = frase1.split()
+print(dividido[0])
+print(dividido[3][1])
