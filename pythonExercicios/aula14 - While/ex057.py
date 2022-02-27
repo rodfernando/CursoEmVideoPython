d64d13e6fa8a3c87776995da59327134eff3fa00
rodfernando <68609581+rodfernando@users.noreply.github.com>
2022-02-27 13:45:09 -0300

    Atualização até a aula 58

--- pythonExercicios/aula14 - While/ex057.py
+++ pythonExercicios/aula14 - While/ex057.py
@@ -1,18 +1,7 @@
 '''Faça um programa que leia o sexo das pessoas, mas que só aceite os valores de 'M' ou 'F'. Caso esteja errado
 peça o valor novamente até ter o correto'''
 
-n = 1
-masc = fem = 0
-sexo = ''
-while n != 'S':
-    while sexo != 'M' or sexo != 'F':
-        sexo = str(input('Digite um sexo: [M/F]: '))
-        if sexo != 'M' or sexo != 'F':
-            sexo = str(input('Você digitou uma opção inválida. Digite novamente um sexo: [M/F]: '))
-        if sexo == 'M':
-            masc += 1
-        else:
-            fem += 1
-    n = str(input('Deseja encerrar a execução? [S/N]')).upper()
-print('Temos {} homem(ns) e {} mulher(es)'.format(masc, fem))
-
+sexo = str(input('Digite um sexo: [M/F]: ')).strip().upper()[0] # Validações para tirar espaço e aumentar a letra pegando somente a primeira letra
+while sexo not in 'MmFf':
+    sexo = str(input('Você digitou uma opção inválida. Digite novamente um sexo: [M/F]: ')).strip().upper()[0]
+print('O sexo {} foi cadastrado com sucesso!'.format(sexo))
---
+++ pythonExercicios/aula14 - While/ex058.py
@@ -0,0 +1,26 @@
+'''Melhore o jogo do desafio 028 onde o computador vai pensar em um número entre 0 e 10
+Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer'''
+
+import random
+from time import sleep
+
+cont = 0
+numeroDaSorte = int(input('Digite um valor entre 0 e 10: '))
+numeroSorteio = random.randint(0, 10)
+print('PROCESSANDO...')
+sleep(1)
+
+while numeroDaSorte != numeroSorteio:
+    print('O número sorteado é {}.'.format(numeroSorteio))
+    print('Você não foi sorteado! =/')
+    cont += 1
+    numeroDaSorte = int(input('Digite um valor entre 0 e 10: '))
+    print('PROCESSANDO...')
+    sleep(1)
+    numeroSorteio = random.randint(0, 10)
+if numeroDaSorte == numeroSorteio:
+    print('Parabéns! Você acertou!')
+    cont += 1
+
+print('Nº de tentativa(s): {} vezes.'.format(cont))
+print('Programa finalizado com sucesso!')
