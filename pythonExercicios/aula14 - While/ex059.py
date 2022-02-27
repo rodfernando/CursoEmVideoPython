3cc5f12671f1e75e9f7f976b63c1ece66034ae0b
rodfernando <68609581+rodfernando@users.noreply.github.com>
2022-02-27 13:52:38 -0300

    Atualização até a aula 58

--- pythonExercicios/aula14 - While/ex059.py
+++ pythonExercicios/aula14 - While/ex059.py
@@ -5,3 +5,18 @@
 [4]novos números
 [5]sair do programa
 Seu programa deverá realizar a operação solicitada em cada caso.'''
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+opção = ''
+while opção != 5:
+    print('''Digite uma opção: 
+    [1]somar
+    [2]multiplicar
+    [3]maior
+    [4]novos números
+    [5]sair do programa''')
+    opção = int(input(''))
+print('Fim do programa')
+
+
