280210391ec5add959ad40cd7503d7b9627cb92f
rodfernando <hazardrodrigo@hotmail.com>
2022-04-16 11:34:17 -0300

    Atualização aula 76

--- pythonExercicios/aula 16 - Tuplas/ex075.py
+++ pythonExercicios/aula 16 - Tuplas/ex075.py
@@ -5,14 +5,15 @@ B)Em que posição foi digitado o número 3
 C)Quais foram os números pares.'''
 
 tupla = (int(input('Digite um valor: ')),
-         int(input('Digite um valor: ')),
-         int(input('Digite um valor: ')),
-         int(input('Digite um valor: ')))
+         int(input('Digite outro valor: ')),
+         int(input('Digite mais um valor: ')),
+         int(input('Digite o último valor: ')))
 print('')
 print('='*40)
+print(f'Você digitou os valores {tupla}')
 print(f'Quantidade(s) de nove: {tupla.count(9)}.')
 print(f'O número 3 está na posição {tupla.index(3)+1}')
-print(f'O(s) valore(s) pares foi(ram)>')
+print(f'O(s) valore(s) pares foi(ram)>', end='')
 for n in tupla:
     if n % 2 == 0:
         print(n, end=' ')
---
+++ pythonExercicios/aula 16 - Tuplas/ex076.py
@@ -0,0 +1,5 @@
+tupla = (('Lápis', 1.75), ('Borracha', 2.00), ('Caderno', 15.90), ('Estojo'))
+
+print('-'*40)
+print('{:^40}'.format('LISTAGEM DE PREÇOS'))
+print('-'*40)
